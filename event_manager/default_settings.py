95b8ad947be964be26c84f08a74e4248e9f20fb6
Luca Ferroni <luca@befair.it>
2014-07-18 21:50:33 +0200

    Configurazione necessaria per django-suit

--- event_manager/default_settings.py
+++ event_manager/default_settings.py
@@ -25,6 +25,12 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
+
+TEMPLATE_CONTEXT_PROCESSORS = TCP + (
+    'django.core.context_processors.request',                                                                                          
+)
+
 
 # Application definition
 
