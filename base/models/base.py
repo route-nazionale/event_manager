4d062a0af9a27fa8d69bbc9cfd85a979745f007e
Luca Ferroni <luca@befair.it>
2014-07-19 18:20:39 +0200

    Modale sul salvataggio delle informazioni

--- base/models/base.py
+++ base/models/base.py
@@ -183,9 +183,6 @@ class HeartBeat(models.Model):
     name = models.CharField(max_length=128, unique=True)
     code = models.IntegerField(blank=True, unique=True)
 
-    class Meta:
-        db_table = "stradecoraggio"
-
     def __unicode__(self):
         return self.name
 
