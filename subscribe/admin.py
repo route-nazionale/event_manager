65b6dcd7133215fb3bc7f17d6f47e786c553a8fc
tapion <tapion@tapion.it>
2014-07-29 12:38:20 +0200

    Visualizzazione iscrizioni capi in admin

--- subscribe/admin.py
+++ subscribe/admin.py
@@ -4,8 +4,7 @@ from subscribe.models import ScoutChiefSubscription
 
 class ScoutChiefSubscriptionAdmin(admin.ModelAdmin):
 
-    list_display = ('__unicode__', 'scout_chief', 'event', 'subscribed_on')
-    list_display = ('__unicode__', 'subscribed_on',)
+    list_display = ('scout_chief', 'event_happening', 'subscribed_on')
     #list_editable = ('scout_chief', 'event')
 
 admin.site.register(ScoutChiefSubscription, ScoutChiefSubscriptionAdmin)
