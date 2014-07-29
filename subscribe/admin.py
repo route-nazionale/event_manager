76ce05e79f1f9df01b9ad02240bbd1b0cf921318
tapion <tapion@tapion.it>
2014-07-29 13:00:04 +0200

    Ricerca per gruppo nelle iscrizioni

--- subscribe/admin.py
+++ subscribe/admin.py
@@ -5,6 +5,7 @@ from subscribe.models import ScoutChiefSubscription
 class ScoutChiefSubscriptionAdmin(admin.ModelAdmin):
 
     list_display = ('scout_chief', 'event_happening', 'subscribed_on')
+    search_fields = ('scout_chief__scout_unit',)
     #list_editable = ('scout_chief', 'event')
 
 admin.site.register(ScoutChiefSubscription, ScoutChiefSubscriptionAdmin)
