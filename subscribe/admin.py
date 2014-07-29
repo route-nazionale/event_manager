c069377640b305d58018620081823986d54ccf88
tapion <tapion@tapion.it>
2014-07-29 14:26:51 +0200

    Miglioramento colonne iscrizioni in admin

--- subscribe/admin.py
+++ subscribe/admin.py
@@ -4,8 +4,30 @@ from subscribe.models import ScoutChiefSubscription
 
 class ScoutChiefSubscriptionAdmin(admin.ModelAdmin):
 
-    list_display = ('scout_chief', 'event_happening', 'subscribed_on')
+    list_display = ('scout_chief', 'turno', 'codice_evento', 'nome_evento', 'quartiere', 'posti', 'subscribed_on')
     search_fields = ('scout_chief__scout_unit',)
     #list_editable = ('scout_chief', 'event')
+
+    def turno(self, obj):
+        return obj.event_happening.timeslot.name
+
+    def codice_evento(self, obj):
+        return obj.event_happening.event.code
+
+    def nome_evento(self, obj):
+        return obj.event_happening.event.name
+
+    def quartiere(self, obj):
+        return obj.event_happening.event.district
+    
+    def posti(self, obj):
+        a = obj.event_happening
+        return "%s/%s" % (a.n_seats, a.event.seats_tot)
+
+    def has_add_permission(self, request):
+        return False
+    
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 admin.site.register(ScoutChiefSubscription, ScoutChiefSubscriptionAdmin)
