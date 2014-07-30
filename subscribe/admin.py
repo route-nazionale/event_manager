18468b92fc42dfe447a46c94d60cc148a2aae6d3
Luca Ferroni <luca@befair.it>
2014-07-30 15:35:00 +0200

    Aggiungi iscrizione capo ad evento

--- subscribe/admin.py
+++ subscribe/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from subscribe.models import ScoutChiefSubscription
-from base.models.event import EventHappening
+from base.models import EventHappening, ScoutChief
 
 class ScoutChiefSubscriptionAdmin(admin.ModelAdmin):
 
@@ -10,7 +10,7 @@ class ScoutChiefSubscriptionAdmin(admin.ModelAdmin):
     #list_editable = ('scout_chief', 'event')
 
     fields = ('scout_chief', 'turno', 'quartiere', 'event_happening') #, 'is_locked')
-    readonly_fields = ('scout_chief', 'quartiere', 'turno')
+    readonly_fields = ['quartiere', 'turno']
 
     def turno(self, obj):
         return obj.event_happening.timeslot.name
@@ -28,11 +28,11 @@ class ScoutChiefSubscriptionAdmin(admin.ModelAdmin):
         a = obj.event_happening
         return "%s/%s" % (a.seats_n_chiefs, a.event.max_chiefs_seats)
 
-    def has_add_permission(self, request):
-        return False
-    
-    def has_delete_permission(self, request, obj=None):
-        return False
+    #def has_add_permission(self, request):
+    #    return False
+    #
+    #def has_delete_permission(self, request, obj=None):
+    #    return False
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         # if we are changing a person and we are not a superuser
@@ -41,10 +41,21 @@ class ScoutChiefSubscriptionAdmin(admin.ModelAdmin):
             if self._obj:
                 eh_qs = eh_qs.filter(timeslot=self._obj.event_happening.timeslot)
             kwargs["queryset"] = eh_qs.order_by("event__print_code")
+        elif db_field.name == "scout_chief":
+            qs = ScoutChief.objects.select_related()
+            kwargs["queryset"] = qs.order_by("scout_unit")
+            
         return super(ScoutChiefSubscriptionAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
     
     def get_form(self, request, obj=None):
         self._obj = obj #Hack to filter formfield_for_foreignkey
+        if self._obj:
+            if 'scout_chief' not in self.readonly_fields:
+                self.readonly_fields.append('scout_chief')
+        else:
+            if 'scout_chief' in self.readonly_fields:
+                self.readonly_fields.remove('scout_chief')
+
         form = super(ScoutChiefSubscriptionAdmin, self).get_form(request, obj)
         return form
 
